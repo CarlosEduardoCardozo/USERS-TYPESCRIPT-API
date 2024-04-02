09c269d78943d5fe922704e485e7e88db1b174be
CarlosEduardoCardozo <cardozogumsc@gmail.com>
2024-04-02 00:41:03 -0300

    feat: add GetUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,20 @@
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+
+export class GetUsersController implements IGetUsersController {
+  constructor(private readonly getUserRepository: IGetUsersRepository) {}
+  async handle() {
+    try {
+      const users = await this.getUserRepository.getUser();
+
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong.",
+      };
+    }
+  }
+}
