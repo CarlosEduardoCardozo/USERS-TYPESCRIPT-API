6a5e59c75fb44f3ed2254f8743ecac4d01864203
CarlosEduardoCardozo <cardozogumsc@gmail.com>
2024-04-02 17:33:11 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/controllers/get-users/get-users.ts
+++ src/controllers/get-users/get-users.ts
@@ -1,10 +1,10 @@
 import { IGetUsersController, IGetUsersRepository } from "./protocols";
 
 export class GetUsersController implements IGetUsersController {
-  constructor(private readonly getUserRepository: IGetUsersRepository) {}
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
   async handle() {
     try {
-      const users = await this.getUserRepository.getUser();
+      const users = await this.getUsersRepository.getUsers();
 
       return {
         statusCode: 200,
--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
-  async getUser(): Promise<User[]> {
-    return [
-      {
-        firstName: "Carlos",
-        lastName: "Carlos",
-        email: "cardozo@gmail.com",
-        password: "123",
-      },
-    ];
+  async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
